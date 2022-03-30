a5fc3cdbece65b648c8e6b0fde268ba7a4c509c7
Din Dmitriy <dmitriy1d01@gmail.com>
2022-03-30 22:03:25 +0300

    Added init test. Added lamports movements in SC

--- programs/donation-platform/src/lib.rs
+++ programs/donation-platform/src/lib.rs
@@ -1,4 +1,7 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_instruction::transfer;
+use anchor_lang::solana_program::program::invoke;
+use anchor_lang;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,29 +9,44 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod donation_platform {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>,
-                      // bump: u8,
-                      target: u64) -> Result<()>
-    {
+    pub fn initialize(ctx: Context<Initialize>, target: u64) -> Result<()> {
+        require!(target > 0, DonateErrors::ZeroLamports);
         let donate_platform = &mut ctx.accounts.donate_platform;
         // donate_platform.bump = bump;
-        donate_platform.authority = *ctx.accounts.authority.to_account_info().key;
+        donate_platform.authority = ctx.accounts.authority.key();
         donate_platform.target = target;
         donate_platform.collected = 0;
         Ok(())
     }
 
     pub fn send(ctx: Context<Send>, amount: u64) -> Result<()>{
-        let address = ctx.accounts.donator.key();
-        let donation = Donation {address, amount};
-        msg!("{:?}", donation);
+        require!(amount > 0, DonateErrors::ZeroLamports);
+
+        let donate_platform = &ctx.accounts.donate_platform;
+        let collected = donate_platform.collected;
+        let target = donate_platform.target;
+        require!(target > collected, DonateErrors::TargetReached);
+
+        let (from, from_info) = (&ctx.accounts.donator.key(), ctx.accounts.donator.to_account_info());
+        let (to, to_info) = (&ctx.accounts.donate_platform.key(), ctx.accounts.donate_platform.to_account_info());
+        invoke(&transfer(from, to, amount), &[from_info, to_info])?;
+
+        let donation = Donation {address: *from, amount};
         let donate_platform = &mut ctx.accounts.donate_platform;
+
         donate_platform.donators.push(donation);
         donate_platform.collected += amount;
         Ok(())
     }
 
     pub fn withdraw(ctx: Context<Withdraw>) -> Result<()>{
+        let collected = ctx.accounts.donate_platform.collected;
+        require!(collected > 0, DonateErrors::NoCollectedLamports);
+
+        let (from, from_info) = (&ctx.accounts.donate_platform.key(), ctx.accounts.donate_platform.to_account_info());
+        let (to, to_info) = (&ctx.accounts.authority.key(), ctx.accounts.authority.to_account_info());
+        invoke(&transfer(from, to, collected), &[from_info, to_info])?;
+
         ctx.accounts.donate_platform.collected = 0;
         Ok(())
     }
@@ -38,10 +56,7 @@ pub mod donation_platform {
 pub struct Initialize<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
-
-    #[account(init, payer = authority, space = 64 + 64)]
-    // seeds = [b"donate_platform", authority.key().as_ref()], bump
-
+    #[account(init, payer = authority, space = 64 + 64, seeds = [b"donate_platform", authority.key().as_ref()], bump)]
     pub donate_platform: Account<'info, Donates>,
     pub system_program: Program<'info, System>
 }
@@ -56,7 +71,7 @@ pub struct Withdraw<'info> {
 #[derive(Accounts)]
 pub struct Send<'info>{
     pub donator: Signer<'info>,
-    // #[account(mut, seeds=[b"donate_platform", receiver.to_bytes()], bump = donate_platform.bump)]
+    // #[account(mut, bump = donate_platform.bump)]
     #[account(mut)]
     pub donate_platform: Account<'info, Donates>
 }
@@ -74,4 +89,15 @@ pub struct Donates {
 pub struct Donation {
     address: Pubkey,
     amount: u64,
+}
+
+#[error_code]
+pub enum DonateErrors {
+    #[msg("Amount of lamports must be more than zero")]
+    ZeroLamports,
+    #[msg("Impossible to withdraw. No lamports were collected")]
+    NoCollectedLamports,
+    #[msg("The target was reached")]
+    TargetReached,
+
 }
--- tests/donation-platform.ts
+++ tests/donation-platform.ts
@@ -1,58 +1,81 @@
 import * as anchor from "@project-serum/anchor";
 import {Program} from "@project-serum/anchor";
 import {DonationPlatform} from "../target/types/donation_platform";
+import chai from "chai";
+import {expect, assert} from "chai";
+import chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
 
 const {SystemProgram} = anchor.web3;
 
-describe("Let's test donation platform!", () => {
+describe("Let's test donation platform!", async () => {
   anchor.setProvider(anchor.Provider.env());
   const provider = anchor.getProvider();
 
   const program = anchor.workspace.DonationPlatform as Program<DonationPlatform>;
-  const donatePlatformKeypair = anchor.web3.Keypair.generate();
-  const donatePlatform = donatePlatformKeypair.publicKey;
-  const donatorKeypair = anchor.web3.Keypair.generate();
-  const donator = donatorKeypair.publicKey;
-  const owner = provider.wallet;
-  const authority = owner.publicKey;
   const systemProgram = SystemProgram.programId;
+  // const donatePlatformKeypair = anchor.web3.Keypair.generate();
+  // const donatePlatform = donatePlatformKeypair.publicKey;
+  let owner = provider.wallet;
+  let authority = owner.publicKey;
 
-  it("Is initialized!", async () => {
-    const tx = await program.methods
+  let [donatePlatform, donatePlatformBump] = await anchor.web3.PublicKey.findProgramAddress(
+    [Buffer.from("donate_platform"), authority.toBuffer()], program.programId
+  )
+
+  let donatorKeypair = anchor.web3.Keypair.generate();
+  let donator = donatorKeypair.publicKey;
+
+  it("Program must throw error with target 0", async () => {
+    await expect((async () =>
+        await program.methods
+          .initialize(new anchor.BN(0))
+          .accounts({
+            donatePlatform,
+            authority,
+            systemProgram,
+          })
+          .rpc()
+      )()
+    ).to.be.rejectedWith(/Amount of lamports must be more than zero/);
+  });
+
+  it("Program is initialized correctly", async () => {
+    let tx = await program.methods
       .initialize(new anchor.BN(1000))
       .accounts( {
           donatePlatform,
           authority,
           systemProgram,
       })
-      .signers([donatePlatformKeypair])
       .rpc();
 
-    const data = await program.account.donates.fetch(donatePlatform);
-    console.log(data);
-    console.log("Your transaction signature", tx);
-  });
-  it("Sends!", async () => {
-    const tx = await program.methods
-      .send(new anchor.BN(1000))
-      .accounts({
-        donatePlatform,
-        donator,
-      })
-      .signers([donatorKeypair])
-      .rpc()
-    console.log('Completed send!,', tx)
-    const data = await program.account.donates.fetch(donatePlatform);
-    console.log(data)
+    let donates = await program.account.donates.fetch(donatePlatform);
+    assert.equal(donates.target, 1000, "Targets are not the same!");
+    assert.deepEqual(donates.authority, authority, "Authorities are not the same!")
+    assert.equal(donates.collected, 0, "Collected amount is not zero!")
   });
 
-  it("Withdraw", async () => {
-    const tx = await program.methods
-      .withdraw()
-      .accounts({donatePlatform, authority})
-      .rpc()
-    const data = await program.account.donates.fetch(donatePlatform);
-    console.log(data)
-
-  });
+  // it("Sends lamports", async () => {
+  //   const tx = await program.methods
+  //     .send(new anchor.BN(100))
+  //     .accounts({
+  //       donator,
+  //       donatePlatform,
+  //     })
+  //     .signers([donatorKeypair])
+  //     .rpc()
+  //   console.log('Completed send!,', tx)
+  //   const data = await program.account.donates.fetch(donatePlatform);
+  //   console.log(data)
+  // });
+  //
+  // it("Withdraw", async () => {
+  //   const tx = await program.methods
+  //     .withdraw()
+  //     .accounts({donatePlatform, authority})
+  //     .rpc()
+  //   const data = await program.account.donates.fetch(donatePlatform);
+  //   console.log(data)
+  // });
 });
