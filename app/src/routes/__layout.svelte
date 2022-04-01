123e51a275558d1a88c990e0f4af2b1696f508d8
Dmitriy <dmitriy1d01@gmail.com>
2022-04-01 16:54:58 +0300

    Started making an app

---
+++ app/src/routes/__layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { clusterApiUrl } from '@solana/web3.js';
+  import { WalletProvider } from '@svelte-on-solana/wallet-adapter-ui';
+  import { AnchorConnectionProvider } from '@svelte-on-solana/wallet-adapter-anchor';
+  import idl from '../../../target/idl/donation_platform.json';
+
+  const localStorageKey = 'walletAdapter';
+  const network = clusterApiUrl('devnet');
+
+  let wallets;
+
+  onMount(async () => {
+    const {
+      PhantomWalletAdapter,
+      SlopeWalletAdapter,
+      SolflareWalletAdapter,
+      SolletExtensionWalletAdapter,
+    } = await import('@solana/wallet-adapter-wallets');
+
+    const walletsMap = [
+      new PhantomWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new SolletExtensionWalletAdapter(),
+    ];
+
+    wallets = walletsMap;
+  });
+</script>
+
+<WalletProvider {localStorageKey} {wallets} autoConnect />
+<AnchorConnectionProvider {network} {idl} />
+<div>
+  <slot />
+</div>
---
+++ app/svelte.config.js
@@ -0,0 +1,27 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+import path from 'path';
+
+const config = {
+  preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+		vite: {
+      optimizeDeps: {
+        include: ['@project-serum/anchor'],
+      },
+      resolve: {
+        alias: {
+          $utils: path.resolve('src/utils/'),
+        },
+      },
+      define: {
+        // This makes @project-serum/anchor 's process error not happen since it replaces all instances of process.env.BROWSER with true
+        'process.env.BROWSER': true
+      },
+    },
+  },
+};
+
+export default config;
